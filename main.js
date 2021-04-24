8d5b965f78e906dde9a72b32a020344b49654f9d
ромусенко <72911652+RAMONEZO@users.noreply.github.com>
2021-04-24 09:55:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+let map = {
+    x: 0,
+    y: 0,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    color: '#f5f5f5'
+}
+
+let cube = {
+    x: 0,
+    y: 0,
+    id: 'cube',
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 56,
+    color: 'black'
+}
+
+let bots = [];
+
+let bot = {
+    x: 0,
+    y: 0,
+    id: 1,
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 56,
+    color: 'red'
+}
+
+// bots.push(spawnCube(map, bot));
+
+let buffs = [];
+
+let buff = {
+    x: 100,
+    y: 100,
+    id: 1,
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 56,
+    color: '#66ff00'
+}
+
+// buffs.push(spawnCube(map, buff));
+
+renderMap(map);
+renderCube(spawnCube(map, cube));
+// renderBots(bots);
+// renderBuffs(buffs);
+
+let tike =setInterval(timeTike, 1000);
